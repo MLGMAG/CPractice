69aeef0025340ecee544fa202fe630be692a3c3d
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2019-09-22 06:34:45 +0300

    myLibrary.h: added (decimalToBinary) function from convertingDecimalToBinary.c

---
+++ myLibrary.h
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+char *decimalToBinary(int decimal) {
+    int arrayLen = 35;
+    static char binaryRepresentation[35];
+    int sing = '0';
+    if (decimal < 0) {
+        sing = '1';
+    }
+    for (int i = arrayLen - 1; i >= 0; --i) {
+        if (i == 26 | i == 17 | i == 8) {
+            binaryRepresentation[i] = ' ';
+            --i;
+        }
+        if (i == 0) {
+            binaryRepresentation[0] = sing;
+        } else if ((decimal >= 1) | (decimal <= -1)) {
+            binaryRepresentation[i] = abs(decimal % 2) + '0';
+            decimal /= 2;
+        } else {
+            binaryRepresentation[i] = '0';
+        }
+    }
+    return binaryRepresentation;
+}
